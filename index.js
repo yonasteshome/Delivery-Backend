688bce40c823b2ce7ff681c4fdca8340b38eba08
Yonas Teshome <140391850+yonasteshome@users.noreply.github.com>
2025-09-19 22:48:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 
 //frontend make the frontend work on port 3000
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:3001",
+  origin: "https://real-time-food-delivery-22-y6nn.vercel.app",
   credentials: true,
 }));
 
